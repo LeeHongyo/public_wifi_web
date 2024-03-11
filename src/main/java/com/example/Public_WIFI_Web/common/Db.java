9ee1943addbc0098f3237c44b2547433920441ee
홍교 <111673196+LeeHongyo@users.noreply.github.com>
2024-03-11 21:46:13 +0900

    fix: setting framwork

--- src/main/java/com/example/Public_WIFI_Web/common/Db.java
+++ src/main/java/com/example/Public_WIFI_Web/common/Db.java
@@ -1,6 +1,6 @@
 package com.example.Public_WIFI_Web.common;
 
 public class Db {
-    public static final String URL = "jdbc:sqlite://Users/suhwan/Desktop/seoul-public-wifi/wifi.db";
-    public static final String CLASS = "org.sqlite.JDBC";
+    public static final String URL = "jdbc:mysql://localhost:3306/your_database_name";
+    public static final String CLASS = "com.mysql.cj.jdbc.Driver";
 }
